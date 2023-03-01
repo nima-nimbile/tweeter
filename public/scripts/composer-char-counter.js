67140ea6d28e65b08e1cbb2eef843d864b935fb2
Nima <nima.mohamed@hotmail.com>
2023-03-01 15:11:56 -0800

    creat tweet and add some style in the project

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,17 @@ $(document).ready(function () {
 
   $(".new-tweet").on( "keyup keypress", "textarea" , function(event) {
    
-    const textarea = event.target;
-    const text = textarea.value;
-    const charsLeft = 140 - text.length;
+    const $textarea = event.target;
+    const $text = $textarea.value;
+    const $charsLeft = 140 - $text.length;
 
-    const counter = textarea.closest("form").querySelector(".counter");
-    counter.textContent = charsLeft;
+    const $counter = $textarea.closest("form").querySelector(".counter");
+    $counter.textContent = $charsLeft;
     
-    if (charsLeft < 0) {
-      counter.style.color = "red";
+    if ($charsLeft < 0) {
+      $counter.style.color = "red";
     } else {
-      counter.style.color = "";
+      $counter.style.color = "";
     }
 
   });
