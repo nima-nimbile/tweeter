be371504b7c2a361a2d43a90c70bbdd1ea167518
Nima <nima.mohamed@hotmail.com>
2023-03-03 15:37:29 -0800

    finishing css part

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,19 +5,6 @@
  * 
  */
 $(document).ready(function () {
-// ..........................................................................................tweetData Object
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// };
 // ..........................................................................................loadTweets
 const loadTweets = function() {
   $.ajax({
@@ -55,8 +42,8 @@ const createTweetElement = (tweetData) => {
   
   const $tweet = (`<article>` +
     `<header>` +
-    `<img class="avatar" src="${tweetData.user.avatar}">` +
-    `<h2 class="avatar">${tweetData.user.name}</h2>` +
+    `<img class="avatar" src="${tweetData.user.avatars}">` +
+    `<h2 class="username">${tweetData.user.name}</h2>` +
     `<span class="handle">${tweetData.user.handle}</span>` +
     `</header>` +
     `<div id ="text-second">${escape(tweetData.content.text)}</div>` +
@@ -78,9 +65,10 @@ const createTweetElement = (tweetData) => {
 
 $('form').on("submit", (event) => {
   console.log("$('form'),", $('form'))
-  event.preventDefault();
+  
   $("#book").slideUp();
-    $("#book1").slideUp();
+  $("#book1").slideUp();
+  event.preventDefault();
   const tweetData = event.target[0].value
   console.log("tweetData1111", tweetData);
   if (tweetData === "" || tweetData === null){
