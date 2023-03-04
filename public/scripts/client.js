a6a6e0f7b3eae9066fca62b0fe04b6726b99347c
Nima <nima.mohamed@hotmail.com>
2023-03-03 21:58:22 -0800

    Finished the tweet project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,91 +5,82 @@
  * 
  */
 $(document).ready(function () {
-// ..........................................................................................loadTweets
-const loadTweets = function() {
-  $.ajax({
-    type: 'GET',
-    url: '/tweets',
-    success: (tweets) => {
-      //  console.log("tweets",tweets);
-       renderTweets(tweets);
+  // ..........................................................................................loadTweets
+  const loadTweets = function () {
+    $.ajax({
+      type: 'GET',
+      url: '/tweets',
+      success: (tweets) => {
+        renderTweets(tweets);
       }
-  })
-};
-loadTweets();
-// ..........................................................................................renderTweets
-const $tweeconainer = $("#tweets-container");
-const renderTweets = (tweets) => {
-      $tweeconainer.empty();
-      for (let data in tweets) {
-        const $tweet = createTweetElement(tweets[data]);
-        $tweeconainer.prepend($tweet);
-      }
-    };
-// ..........................................................................................escape = function (str)
-    const escape = function (str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-// ..........................................................................................createTweetElement = (tweetData)
-const createTweetElement = (tweetData) => {
-  console.log("tweetData",tweetData);
-  const dateCreated = new Date(tweetData.created_at);
-  const dateToday = new Date();
-  const timeDiff = Math.abs(dateToday.getTime() - dateCreated.getTime());
-  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-  
-  const $tweet = (`<article>` +
-    `<header>` +
-    `<img class="avatar" src="${tweetData.user.avatars}">` +
-    `<h2 class="username">${tweetData.user.name}</h2>` +
-    `<span class="handle">${tweetData.user.handle}</span>` +
-    `</header>` +
-    `<div id ="text-second">${escape(tweetData.content.text)}</div>` +
-    `<footer>` +
-    `<span class="dayago">${diffDays} days ago</span>` +
-    `<div class="icons">` +
-    `<span class="flag"><i class="fa fa-flag" aria-hidden="true"></i></span>` +
-    `<span class="rotate"><i class="fa fa-retweet" aria-hidden="true"></i></span>` +
-    `<span class="heart""><i class="fa fa-heart" aria-hidden="true"></i></span>` +
-    `</div>` +
-    `</footer>` +
-    `</article>`);
-    console.log("$tweet", $tweet);
-
-  return $tweet;
-
-};
-// ..........................................................................................POST req
-
-$('form').on("submit", (event) => {
-  console.log("$('form'),", $('form'))
-  
-  $("#book").slideUp();
-  $("#book1").slideUp();
-  event.preventDefault();
-  const tweetData = event.target[0].value
-  console.log("tweetData1111", tweetData);
-  if (tweetData === "" || tweetData === null){
-   return  $("#book").slideDown('slow');
-  }
-  if(tweetData.length > 140){
-    return $("#book1").slideDown('slow');
-    } 
-  const urlEncode = $('form').serialize();
-  $.ajax({
-    method: 'POST',
-    url: "/tweets",
-    data: urlEncode,
-    success: (response) => {
-      console.log("response1", response);
-      loadTweets();
+    })
+  };
+  loadTweets();
+  // ..........................................................................................renderTweets
+  const tweeconainer = $("#tweets-container");
+  const renderTweets = (tweets) => {
+    $('#tweet-text').val("");
+    for (let data in tweets) {
+      const $tweet = createTweetElement(tweets[data]);
+      tweeconainer.prepend($tweet);
     }
+  };
+  // ..........................................................................................escape = function (str)
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  // ..........................................................................................createTweetElement = (tweetData)
+  const createTweetElement = (tweetData) => {
+    console.log("tweetData", tweetData);
+    const dateCreated = new Date(tweetData.created_at);
+    const dateToday = new Date();
+    const timeDiff = Math.abs(dateToday.getTime() - dateCreated.getTime());
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    const $tweet = (`<article>` +
+      `<header>` +
+      `<img class="avatar" src="${tweetData.user.avatars}">` +
+      `<h2 class="username">${tweetData.user.name}</h2>` +
+      `<span class="handle">${tweetData.user.handle}</span>` +
+      `</header>` +
+      `<div id ="text-second">${escape(tweetData.content.text)}</div>` +
+      `<footer>` +
+      `<span class="dayago">${diffDays} days ago</span>` +
+      `<div class="icons">` +
+      `<span class="flag"><i class="fa fa-flag" aria-hidden="true"></i></span>` +
+      `<span class="rotate"><i class="fa fa-retweet" aria-hidden="true"></i></span>` +
+      `<span class="heart""><i class="fa fa-heart" aria-hidden="true"></i></span>` +
+      `</div>` +
+      `</footer>` +
+      `</article>`);
+    return $tweet;
+  };
+
+  // ..........................................................................................POST req
+
+  $('form').on("submit", (event) => {
+    event.preventDefault();
+    $("#book").slideUp();
+    $("#book1").slideUp();
+    const tweetData = event.target[0].value;
+    if (tweetData === "" || tweetData === null) {
+      return $("#book").slideDown('slow');
+    }
+    if (tweetData.length > 140) {
+      return $("#book1").slideDown('slow');
+    }
+    const urlEncode = $('form').serialize();
+    $.ajax({
+      method: 'POST',
+      url: "/tweets",
+      data: urlEncode,
+      success: (response) => {
+        console.log("response1", response);
+        loadTweets();
+      }
+    })
   })
-
-})
-
 });
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,14 @@
 $(document).ready(function () {
-
-  $(".new-tweet").on( "input", function(event) {
-   
+  $(".new-tweet").on("input", function (event) {
     const $textarea = event.target;
     const $text = $textarea.value;
     const $charsLeft = 140 - $text.length;
-
     const $counter = $textarea.closest("form").querySelector(".counter");
     $counter.textContent = $charsLeft;
-    
     if ($charsLeft < 0) {
       $counter.style.color = "red";
     } else {
       $counter.style.color = "";
     }
-
-  });
-
+  })
 });
